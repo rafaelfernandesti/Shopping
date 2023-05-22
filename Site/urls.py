b7e48728c636270c833bb239cac64da8f3f8d5af
Rafael <rafa.fernan10@gmail.com>
2023-05-22 20:47:09 -0300

    Criação de páginas adicionais

--- Site/urls.py
+++ Site/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name = "index"),
     path('produtos', views.produto_lista, name="produto_lista"), #listagem de todos os produtos
     path('produto', views.produto_detalhe, name="produto_detalhe"), #detalhe de um produto específico
+    path('sobre-a-empresa', views.institucional, name="institucional"), #sobre a empresa
+    path('cadastro', views.cadastro, name="cadastro"), #cadastro
+    path('contato', views.contato, name="contato"), #contato
 ]
