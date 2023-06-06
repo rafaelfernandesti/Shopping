a5e67723bcb49d24dbb2de6c9f5b1eb99c9c4cca
Rafael Fernandes <rafa.fernan10@gmail.com>
2023-06-06 15:21:27 -0300

    Ajuste no host do Database - Settings.py

--- Shopping/settings.py
+++ Shopping/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
          'NAME': 'railway',
          'USER': 'root',
          'PASSWORD': 'DaD72kAErwwe0R8mLvRz',
-         'HOST': 'https://containers-us-west-141.railway.app',
+         'HOST': 'containers-us-west-141.railway.app',
          'PORT': '6796',
      }
 }
